2b5ea6d6dc4d0443ca35948384925d662e4fd483
ChristianAEDev <ChristianAE.DEV@gmail.com>
2017-10-21 19:28:19 +0100

    #12 Render icon in timeline depending on it's type

--- src/tasks/TaskUpdates.js
+++ src/tasks/TaskUpdates.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Timeline, TimelineEvent } from 'react-event-timeline';
 import moment from 'moment';
-import AddIcon from 'material-ui-icons/Add';
+import PhoneIcon from 'material-ui-icons/PermPhoneMsg';
+import FileIcon from 'material-ui-icons/Description';
+import MailIcon from 'material-ui-icons/Email';
 import { DATE_TIME_FORMAT } from '../Defines';
 
 /**
  * Component to display all updates of a task.
  */
 class TaskUpdates extends Component {
+  /**
+   * Determines which icon to render depending on the type of the update
+   */
+  getIcon = (type) => {
+    switch (type) {
+      case 'PHONE_CALL':
+        return <PhoneIcon />;
+      case 'FILE':
+        return <FileIcon />;
+      case 'MAIL':
+        return <MailIcon />;
+      default:
+        return <div />;
+    }
+  };
+
   /**
    * Render method
    */
@@ -21,7 +39,8 @@ class TaskUpdates extends Component {
             <TimelineEvent
               title={update.title}
               createdAt={moment(update.timestamp).format(DATE_TIME_FORMAT)}
-              icon={<AddIcon />}
+              icon={this.getIcon(update.type)}
+              iconStyle={{ marginLeft: 0, marginTop: 0 }}
               iconColor="#5C6BC0"
             >
               {update.description}
